f95dcd9ae5852f891db714698301638fc2f7d167
shubhamchauhan1204.99@gmail.com <shubhamchauhan1204.99@gmail.com>
2023-06-03 00:16:54 +0530

    final commit

--- TextUtils/settings.py
+++ TextUtils/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w3-+=^@t*ra^z)6*4b)lrcwa^lns158v*-z!q@iirp9*r*%^sr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False  # has to ber true 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1'] # maually adede
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+      "whitenoise.middleware.WhiteNoiseMiddleware", # maually added
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
